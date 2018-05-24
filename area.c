4ca4705d6a45a0c52a9371676d97d7af2ea1a004
tsaisandesh95 <saisandeshtunuguntl@gmail.com>
2018-05-24 15:37:59 -0500

    First Commit

---
+++ area.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+#define length 200
+int perimeter (int,int);
+
+int main()
+{
+    int area,breadth,p;
+    printf ("enter breadth values :\t ");
+    scanf("%d",&breadth);
+    area=length * breadth;
+    p = perimeter(length,breadth);
+    printf("area of rectangle is  %d",area);
+    printf ("perimeter of rectangle is %d",p);
+}
+
+int perimeter(int a, int b)
+{
+    int c;
+    
+    c= 2*(a+b);
+return c;
+}
+
